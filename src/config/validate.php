0cf272b026b3bda0f588d0ad4694418490a9a73d
kanelli <kanelli@tencent.com>
2023-03-05 22:00:32 +0800

    优化配置参数

--- src/config/validate.php
+++ src/config/validate.php
@@ -1,9 +1,6 @@
 <?php
 return [
-    'graph' =>
-        [
-            'rand_number' => env('RAND_NUMBER', '1234567890'),
-            'width' => env('WIDTH', 100),
-            'height' => env('HEIGHT', 40),
-        ],
+    'rand_number' => env('RAND_NUMBER', '1234567890'),// 随机因子
+    'width' => env('WIDTH', 60),
+    'height' => env('HEIGHT', 40),
 ];
